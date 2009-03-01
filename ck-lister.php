cab2dbb218626495435a68a76aab4129a2ca5b37
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-03-01 00:53:49 -0700

    First commit

---
+++ ck-lister.php
@@ -0,0 +1,23 @@
+<?php // ck-lister v0.1.0 by, Chris Kankiewicz (http://www.chriskankiewicz.com)
+
+  //define the path as relative
+  $path = getcwd();
+
+  //using the opendir function
+  $dir_handle = @opendir($path) or die("Unable to open $path");
+
+  echo "Directory Listing of $path<br/>";
+
+  //running the while loop
+  while ($file = readdir($dir_handle)) {
+    if ($file !== '.') {
+      echo "<a href='$file'>$file</a><br/>";
+    }
+  }
+  
+  echo $path;
+
+  //closing the directory
+  closedir($dir_handle);
+
+?>
