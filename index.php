1bd2295afdf9b025a54a5a03a2378b5010d956b3
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-03-02 17:18:52 -0700

    Multi-level directory listing impimented

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
   // Get path if set otherwise get relative path
   if (isset($_GET['dir'])) {
-    $path = $_GET['dir'];
+    $dir = $_GET['dir'];
   } else {
-    $path = '.';
+    $dir = '.';
   }
-  
+
+  $path = $path . $dir;
+
   if(substr($path,-1,1) != '/') {
     $path = $path . '/';
 	}
@@ -28,7 +30,7 @@
   }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -49,53 +51,73 @@
     <span class="file-size">Size</span>
     <span class="file-time">Last Modified</span>
   </div>
-  
+
   <!-- BEGIN DIRECTORY LISTING -->
 <?php // Echo directory list list
   for ($x = 0; $x < count($fileArray); $x++) {
-    
+
     // Set varriables
-    $icon = 'blank.png';
     $name = $fileArray[$x][name];
     $size = $fileArray[$x][size];
     $time = date("Y-m-d H:i:s", $fileArray[$x][time]);
-    
+
     // Define file extension and the associated image
-    $filetypes = array (
-      'exe' => 'app.png',
+    $fileIcons = array (
+      // Video
+      'avi' => 'video.png',
+      'wmv' => 'video.png',
+      'mp4' => 'video.png',
+      
+      // Audio
+      'wav' => 'music.png',
+      'wma' => 'music.png',
+      'mp3' => 'music.png',
+      'ogg' => 'music.png',
+      
+      // Images
       'gif' => 'image.png',
+      'jpeg' => 'image.png',
+      'jpg' => 'image.png',
+      'png' => 'image.png',
+      
+      // Code
+      'css' => 'code.png',
       'htm' => 'code.png',
       'html' => 'code.png',
-      'iso' => 'cd.png',
-      'jpeg' => 'image.png',
-      'jpg' => 'image.png', 
       'php' => 'code.png',
-      'png' => 'image.png',
+      
+      // Applications
+      'exe' => 'app.png',
+      'msi' => 'app.png',
+      
+      // Other
+      'iso' => 'cd.png',
       'txt' => 'text.png',
-      'zip' => 'archive.png'
+      'zip' => 'archive.png',
     );
-    
+
     // Set icon if of a valid extension
 		$ext = strtolower(substr($name, strrpos($name, '.')+1));
-    if($filetypes[$ext]) {
-			$icon = $filetypes[$ext];
-		} 
-    
-  
+    if($fileIcons[$ext]) {
+			$icon = $fileIcons[$ext];
+		} else {
+      $icon = 'blank.png';
+    }
+
     if (isOdd($x)) {
       echo("  <div class=\"light-bg\">\r\n");
     } else {
       echo("  <div class=\"dark-bg\">\r\n");
     }
-    
-    if (is_dir($name)) {
+
+    if (is_dir("$path/$name")) {
       echo("    <a href=\"?dir=$path$name\">\r\n");
     } else {
       echo("    <a href=\"$path$name\">\r\n");
     }
-    
+
     echo("      <img src=\"ck-lister/icons/$icon\" />\r\n");
-    
+
     echo('      <span class="file-name">'.$name."</span>\r\n");
     echo('      <span class="file-size">'.$size."KB</span>\r\n");
     echo('      <span class="file-time">'.$time."</span>\r\n");
@@ -104,15 +126,15 @@
   }
 ?>
   <!-- END DIRECTORY LISTING -->
-  
+
   <div id="ck-footer">
     <span class="footer-left">
-      &copy; 2009 
-      <strong>&middot;</strong> Chris Kankiewicz 
+      &copy; 2009
+      <strong>&middot;</strong> Chris Kankiewicz
       <strong>&middot;</strong> Some Rights Reserved
     </span>
     <span class="footer-right">
-      Powered by, 
+      Powered by,
       <a href="http://github.com/PHLAK/ck-lister">CK-Lister</a>
     </span>
   </div>
